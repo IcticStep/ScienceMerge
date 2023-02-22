0a92805798faa1825b1bf69f432598e08a0bb77c
Maxim Moskalyk <ictic.step@gmail.com>
2023-02-22 17:32:13 +0200

    Card visual apperence added.

---
+++ Assets/Scripts/Model/Card.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Model
+{
+    public class Card
+    {
+        public string Name { get; }
+        public TimeSpan MergeTime { get; }
+        public int Cost { get; }
+    }
+}
